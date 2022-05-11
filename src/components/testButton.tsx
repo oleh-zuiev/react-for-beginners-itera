a384019d133e8f77fbadb1da2bd482b8baedc5da
oleh-zuiev <64410055+oleh-zuiev@users.noreply.github.com>
2022-05-11 09:50:42 +0300

    upd

--- src/components/testButton.tsx
+++ src/components/testButton.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 const TestButton = (props: { log: string }) => {
     const [name, setState] = useState('morning');
+    const [dayOfWeek, setDay] = useState('Monday\'s')
     const handleClick = () => {
         setState('night');
+        setDay('Tuesday\'s')
         console.log(props.log);
     };
 
     return (
         <div>
+            <p>{dayOfWeek}</p>
             <p>{name}</p>
             <button onClick={handleClick}>{props.log}</button>
         </div>
